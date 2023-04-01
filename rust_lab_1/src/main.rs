0ac989407bbb89847ab55b036b6f404530662ed1
Toni Rusu <tonirusu@microsoft.com>
2023-04-01 18:14:06 +0300

    Converted map to 2D + small fixes

--- rust_lab_1/src/main.rs
+++ rust_lab_1/src/main.rs
@@ -4,10 +4,19 @@ use std::io;
 
 
 const GRID_SIZE_X:usize = 5;
-const GRID_SIZE_Y:usize = 5;
+const GRID_SIZE_Y:usize = 4;
 const START_HEALTH:i32 = 3;
 const START_EXPLOSIONS:i32 = 36;
 const EXPLOSIONS_LOST_ON_FREEZE:i32 = 3;
+
+
+/// Game status.
+#[derive(Clone, Copy, PartialEq, Eq)]
+enum GameStatus {
+    Running,
+    YouWon,
+    BrotherWon,
+}
 
 
 /// What can occupy a space in the grid.
@@ -16,7 +25,6 @@ enum GridOccupant {
     Tree,
     Player,
     Sister,
-    // TODO Bombed ??
 }
 
 impl fmt::Display for GridOccupant {
@@ -28,7 +36,6 @@ impl fmt::Display for GridOccupant {
             GridOccupant::Tree   => '_',
             GridOccupant::Player => 'P',
             GridOccupant::Sister => 'S',
-            //GridOccupant::Bombed => '*',
         };
         write!(f, "{}", c)
     }    
@@ -50,21 +57,17 @@ impl Coord {
             y: rng.gen_range(0..GRID_SIZE_Y),
         }
     }
-
-    #[inline]
-    fn to_index(&self) -> usize {
-        self.y * GRID_SIZE_X + self.x        
-    }
 }
 
 
 /// The Representation of the hide and seek game
 struct GameState {
-    // TODO: make this 2d
-    map: Vec<GridOccupant>,
+    map: Vec<Vec<GridOccupant>>,
     your_health: i32,
     bro_expl_cnt: i32,
+    // - remember bombed positions (until bro gets a hit => we move => bombed positions are reset)
     blast_history: Vec<Coord>,
+    // - remember if bro bombed sis (so he doesn't bomb her twice)
     bombed_sis_pos: Option<Coord>,
 }
 
@@ -73,95 +76,106 @@ impl GameState {
     /// Initializes the GameState
     pub fn new() -> Self {
         let mut game = GameState {
-            map: vec![GridOccupant::Tree; GRID_SIZE_X * GRID_SIZE_Y],
+            map: vec![],
             your_health: START_HEALTH,
             bro_expl_cnt: START_EXPLOSIONS,
             blast_history: vec![],
             bombed_sis_pos: Option::None,
         };
+        // init board
+        (0..GRID_SIZE_Y).for_each(|_| {
+            let row: Vec<GridOccupant> = vec![GridOccupant::Tree; GRID_SIZE_X];
+            game.map.push(row);
+        });
         // place yourself
         let pos = game.random_pos_in_forest();
-        game.map[pos.to_index()] = GridOccupant::Player;
+        game.map[pos.y][pos.x] = GridOccupant::Player;
         // place sister
         let pos = game.random_pos_in_forest();
-        game.map[pos.to_index()] = GridOccupant::Sister;
+        game.map[pos.y][pos.x] = GridOccupant::Sister;
         // done
         game
     }
 
-    pub fn still_running(&self) -> bool {
-        (self.bro_expl_cnt > 0) && (self.your_health > 0)
-    }
-
-    pub fn you_won(&self) -> bool {
-        (self.bro_expl_cnt <= 0) && (self.your_health > 0)
+    pub fn status(&self) -> GameStatus {
+        if self.your_health <= 0 {
+            GameStatus::BrotherWon
+        }
+        else if self.bro_expl_cnt <= 0 {
+            GameStatus::YouWon
+        }
+        else {
+            GameStatus::Running
+        }
     }
 
     pub fn print_map(&self) {
         for y in 0..GRID_SIZE_Y {
             for x in 0..GRID_SIZE_X {
                 let pos = Coord {x, y};
-                print!("{}", self.map[pos.to_index()]);
+                print!("{}", self.map[pos.y][pos.x]);
             }
             println!("");
         }
     }
 
-    // TODO improvements:
-    // - remember bombed positions (until bro gets a hit => we move => bombed positions are reset)
-    // - remember if bro bombed sis (so he doesn't bomb her twice)
     pub fn perform_step(&mut self) {
         let pos = self.pick_position_to_bomb();
         println!("Your brother is attacking ({}, {})!", pos.x, pos.y);
         self.bro_expl_cnt -= 1;
-        match self.map[pos.to_index()] {
-            GridOccupant::Player => {
-                println!("Your brother hit you!");
-                self.your_health -= 1;
-                // move away
-                let new_pos = self.random_pos_in_forest();
-                self.map[pos.to_index()] = GridOccupant::Tree;
-                self.map[new_pos.to_index()] = GridOccupant::Player;
-                // reset the blast history
-                self.blast_history.clear();
-                if let Some(sis_pos) = self.bombed_sis_pos.as_ref() {
-                    self.blast_history.push(sis_pos.clone());
-                }
-            },
-            GridOccupant::Sister => {
-                println!("Your brother hit your sister and was frozen!");
-                self.bro_expl_cnt -= EXPLOSIONS_LOST_ON_FREEZE;
-                self.bombed_sis_pos = Some(pos.clone());
-                self.blast_history.push(pos);
-            },
-            GridOccupant::Tree => {
-                println!("Your brother missed completely!");
-                self.blast_history.push(pos);
-            },
+        match self.map[pos.y][pos.x] {
+            GridOccupant::Player => self.brother_hit_player(pos),
+            GridOccupant::Sister => self.brother_hit_sister(pos),
+            GridOccupant::Tree   => self.brother_hit_nothing(pos),
         }
         println!("Your health: {}", self.your_health);
         println!("Brother's blasts left: {}", self.bro_expl_cnt);
     }
 
-    fn random_pos_conditioned(&self, cond: fn(GridOccupant) -> bool) -> Coord {
+    fn brother_hit_player(&mut self, pos: Coord) {
+        println!("Your brother hit you!");
+        self.your_health -= 1;
+        // move away
+        let new_pos = self.random_pos_in_forest();
+        self.map[pos.y][pos.x] = GridOccupant::Tree;
+        self.map[new_pos.y][new_pos.x] = GridOccupant::Player;
+        // reset the blast history
+        self.blast_history.clear();
+        if let Some(sis_pos) = self.bombed_sis_pos.as_ref() {
+            self.blast_history.push(sis_pos.clone());
+        }
+    }
+
+    fn brother_hit_sister(&mut self, pos: Coord) {
+        println!("Your brother hit your sister and was frozen!");
+        self.bro_expl_cnt -= EXPLOSIONS_LOST_ON_FREEZE;
+        self.bombed_sis_pos = Some(pos.clone());
+        self.blast_history.push(pos);
+    }
+
+    fn brother_hit_nothing(&mut self, pos: Coord) {
+        println!("Your brother missed completely!");
+        self.blast_history.push(pos);
+    }
+
+    fn random_pos_conditioned(&self, cond: fn(&GameState, &Coord) -> bool) -> Coord {
         let mut coord = Coord::random();
-        //while !cond(self.map[coord.to_index()]) {
-        while !cond(self.map[coord.to_index()]) {
+        while !cond(&self, &coord) {
             coord = Coord::random();
         }
         coord
     }
 
     fn random_pos_in_forest(&self) -> Coord {
-        self.random_pos_conditioned(|go| go == GridOccupant::Tree)
+        self.random_pos_conditioned(|game, coord|
+            game.map[coord.y][coord.x] == GridOccupant::Tree
+        )
     }
 
     fn pick_position_to_bomb(&self) -> Coord {
-        let mut coord = Coord::random();
-        while self.blast_history.contains(&coord) {
-            coord = Coord::random();
-        }
-        coord
+        self.random_pos_conditioned(|game, coord|
+            !game.blast_history.contains(&coord)
+        )
     }
 }
 
@@ -173,7 +187,7 @@ impl GameState {
 fn main() {
     let mut buffer = String::with_capacity(256);
     let mut game = GameState::new();
-    while game.still_running() {
+    while game.status() == GameStatus::Running {
         game.print_map();
         game.perform_step();
         // read line
@@ -184,7 +198,7 @@ fn main() {
     }
 
     // game over
-    if game.you_won() {
+    if game.status() == GameStatus::YouWon {
         println!("Congratulations, you WON :)");
     }
     else {
