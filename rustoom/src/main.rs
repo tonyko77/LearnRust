327acf2ad4f4059e966047f08e3cf23adc00ccec
Antonio Rusu <antoniorusu@hotmail.com>
2023-04-22 17:14:44 +0300

    Added Rustoom prj skeleton

---
+++ rustoom/src/main.rs
@@ -0,0 +1,12 @@
+//======================================================================================
+//  DIY Doom Engine, written in Rust :)
+//
+// -> see Recreating DOOM on YouTube:
+//          https://www.youtube.com/playlist?list=PLi77irUVkDasNAYQPr3N8nVcJLQAlANva
+// -> see DIY Doom on GitHub:
+//          https://github.com/amroibrahim/DIYDoom
+//======================================================================================
+
+fn main() {
+    println!("Hello, world!");
+}
