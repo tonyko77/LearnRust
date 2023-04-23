401abed00f10dbb560c5e6dd2272b87f1754bcc8
Antonio Rusu <antoniorusu@hotmail.com>
2023-04-23 18:09:38 +0300

    minigrep: finished implementation

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -2,7 +2,7 @@
 // Minigrep - main functionality
 //--------------------------------
 
-use std::{fs, error::Error};
+use std::{fs, env, error::Error};
 
 
 //-------------------
@@ -11,6 +11,7 @@ use std::{fs, error::Error};
 pub struct Config {
     query: String,
     file_path: String,
+    ignore_case: bool,
 }
 
 
@@ -24,7 +25,13 @@ impl Config {
             // (but later we should try to use references or other "smart" idioms)
             let query = args[1].clone();
             let file_path = args[2].clone();
-            Ok(Self { query, file_path })
+            let ignore_case = env::var("IGNORE_CASE").is_ok();
+            
+            Ok(Self {
+                query,
+                file_path,
+                ignore_case
+            })
         }
     }
 }
@@ -35,7 +42,12 @@ impl Config {
 
 pub fn run(config: &Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(&config.file_path)?;
-    let results = search(config.query.as_str(), contents.as_str());
+    let results = if config.ignore_case {
+        search_case_insensitive(config.query.as_str(), contents.as_str())
+    }
+    else {
+        search_case_sensitive(config.query.as_str(), contents.as_str())
+    };
 
     if results.is_empty() {
         println!("`{}` was not found in file {}", config.query, config.file_path);
@@ -52,10 +64,25 @@ pub fn run(config: &Config) -> Result<(), Box<dyn Error>> {
 }
 
 
-pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+pub fn search_case_sensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let mut results: Vec<&'a str> = Vec::new();
+
     for line in contents.lines() {
         if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results: Vec<&'a str> = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
             results.push(line);
         }
     }
@@ -85,6 +112,7 @@ pub fn extract_program_name<'a>(args: &'a [String]) -> &'a str {
 #[cfg(test)]
 mod tests {
     use super::*;
+
     const TEST_CONTENTS: &'static str = "\
 Rust:
 safe, fast, productive.
@@ -93,14 +121,36 @@ Pick three.";
     #[test]
     fn one_result() {
         let query = "duct";
-        let results = search(query, TEST_CONTENTS);
+        let results = search_case_sensitive(query, TEST_CONTENTS);
         assert_eq!(vec!["safe, fast, productive."], results);
     }
 
     #[test]
     fn no_result() {
         let query = "x";
-        let results = search(query, TEST_CONTENTS);
+        let results = search_case_sensitive(query, TEST_CONTENTS);
         assert_eq!(Vec::<&str>::new(), results);
     }
+
+    const CS_TEST_CONTENTS: &'static str = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.
+Duct tape.";
+
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let results = search_case_sensitive(query, CS_TEST_CONTENTS);
+        assert_eq!(vec!["safe, fast, productive."], results);
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let results = search_case_insensitive(query, CS_TEST_CONTENTS);
+        assert_eq!(vec!["Rust:", "Trust me."], results);
+    }
+
 }
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -12,15 +12,15 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         let cmd = minigrep::extract_program_name(&args);
-        println!("Usage: {cmd} <query string> <path/to/file>");
+        eprintln!("Usage: {cmd} <query string> <path/to/file>");
         process::exit(1);
     });
 
     let result = minigrep::run(&config);
     if let Err(e) = result {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
